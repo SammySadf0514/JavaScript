ba43c7fbe9d38e3b77c23a7f9e942b97154af88d
SammySadf0514 <sameersadar@gmail.com>
2024-03-18 01:44:53 +0530

    First time JavaScript

---
+++ index.js
@@ -0,0 +1,12 @@
+let student ={
+    Mohan: 90,
+    Sameer: 78,
+    Shafiya: 99,
+    Sadar: 90,
+};
+for(let a in student){
+    console.log(student[a])
+}
+for(let b in student){
+    console.log(b)
+}
